73371b67ba5009efcf26849b22f633c9cd4e2a31
JuniorProg <working91@mail.ru>
2015-06-19 23:50:04 +0400

    Подключение Layout и работа с мета тегами, стилями, скриптами

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,42 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    protected function _initView(){//Работа с layout
 
+        $this->bootstrap('layout'); // Загружаем layout в bootstrap.php
+        $layout = $this->getResource('layout'); // Достаем layout
+        $view = $layout->getView();
+
+        $view->doctype('XHTML1_TRANSITIONAL');
+        $view->headTitle('Zend Framework')
+                    ->setSeparator(' - ');
+
+        $view->headMeta()->appendHttpEquiv('Content-type','text/html;charset-utf8') // Мета Теги
+                            ->appendName('author','bloodrain777');
+
+
+        /**
+         * Подключение стилей
+         * appendStylesheet - 1 аргумент: путь к файлу от каталога public
+         * appendStylesheet - 2й : устройства для которых подключается css
+         */
+
+        $view->headLink()->appendStylesheet('/css/blueprint/screen.css','screen,projection')
+                         ->appendStylesheet('/css/blueprint/print.css','print')
+                         ->appendStylesheet('/css/blueprint/ie.css','screen,projection','ie');
+
+        /**********************/
+
+
+        $view->headScript()->appendFile('/js/jquery-1.11.3.min.js'); // Подключаем jQuery
+
+        $view->headLink(array(
+                'href' => 'favicon.png',
+                'rel'   => 'icon'
+
+
+        ));
+    }
 
 }
 
